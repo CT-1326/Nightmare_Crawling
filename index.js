e451c04ddfd72ed822926c1937e637b363083ebf
BSAA7567 <33346331+BSAA7567@users.noreply.github.com>
2020-07-06 12:04:23 +0900

    Revert "Revert "연합뉴스 크롤링""
    This reverts commit 48e20262c0d8d5971694d27817d03b6e71c6d8ee.

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: true })
+const nightmare = Nightmare({show: true})
+
+// nightmare   .goto('https://duckduckgo.com')
+// .type('#search_form_input_homepage', 'github nightmare')
+// .click('#search_button_homepage')   .wait('#r1-0 a.result__a')   .evaluate(()
+// => document.querySelector('#r1-0 a.result__a').href)   .end()
+// .then(console.log)   .catch(error => {     console.error('Search failed:',
+// error)   })
 
 nightmare
-  .goto('https://duckduckgo.com')
-  .type('#search_form_input_homepage', 'github nightmare')
-  .click('#search_button_homepage')
-  .wait('#r1-0 a.result__a')
-  .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
-  .end()
-  .then(console.log)
-  .catch(error => {
-    console.error('Search failed:', error)
-  })
+    .goto("https://www.yna.co.kr/news?site=navi_news")
+    .evaluate(
+        () => document.querySelector('#container > div > div > div.section01 > section > div.list-type038').outerText
+    )
+    .end()
+    .then(res => {
+        console.log(res);
+    })
+    .catch(e => {
+        console.error(e);
+    });
